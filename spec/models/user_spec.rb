6bc74328bd26438a8b46ea3cbb989f58d763b82e
David <davidsg1717@gmail.com>
2018-04-15 21:14:55 -0300

    adding initial tests to users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = FactoryGirl.build(:user) }
+  context 'users attributes' do
+    it 'should respond to email' do
+      expect(@user).to respond_to(:email)
+    end
+
+    it 'should responde to name' do
+      expect(@user).to respond_to(:name)
+    end
+
+    it 'should responde to passwords and password_confirmation' do
+      expect(@user).to respond_to(:password)
+      expect(@user).to respond_to(:password_confirmation)
+    end
+
+    it 'should be valid' do
+      expect(@user).to be_valid
+    end
+  end
 end
