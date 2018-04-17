6ed6b21e1fb2bcc881f3f0bd29a12d23730c2044
David <davidsg1717@gmail.com>
2018-04-17 16:54:02 -0300

    add user token

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   validates_presence_of :email
-  validates_uniqueness_of :email
+  validates_uniqueness_of :email, :auth_token
+  before_create :generate_auth_token!
+
+  def info
+    "#{self.email} - #{self.created_at} - Token: #{Devise.friendly_token}"
+  end
+
+  def generate_auth_token!
+    begin
+      self.auth_token = Devise.friendly_token
+    end while User.exists?(auth_token: self.auth_token)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,6 +10,34 @@ RSpec.describe User, type: :model do
       is_expected.to validate_uniqueness_of(:email).case_insensitive
       is_expected.to validate_confirmation_of(:password)
       is_expected.to allow_value('email@provider.com').for(:email)
+      is_expected.to validate_uniqueness_of(:auth_token)
+    end
+  end
+
+  describe '#info' do
+    it 'returns email and created_at' do
+      user.save!
+      allow(Devise).to receive(:friendly_token).and_return('asdf')
+
+      expect(user.info).to eq "#{user.email} - #{user.created_at} - Token: asdf"
+    end
+  end
+
+  describe '#generate_auth_token!' do
+    it 'generate a unique token' do
+      allow(Devise).to receive(:friendly_token).and_return('asdf')
+
+      user.generate_auth_token!
+
+      expect(user.auth_token).to eq 'asdf'
+    end
+
+    it 'generate another token with first token already has been taken' do
+      allow(Devise).to receive(:friendly_token).and_return('qwer', 'qwer', '1234')
+      existing_user = create(:user, auth_token: 'qwer')
+
+      user.generate_auth_token!
+      expect(user.auth_token).not_to eq (existing_user.auth_token)
     end
   end
 end
